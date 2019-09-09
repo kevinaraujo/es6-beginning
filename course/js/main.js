b9f7dc50ef3d7e9cf92ec23518694ce620fc4f58
Kevin Araújo <kevin.araujo@pravaler.com.br>
2019-09-09 10:25:33 -0300

    adding class

--- course/js/main.js
+++ course/js/main.js
@@ -1,10 +1,53 @@
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Client = function Client(name, email) {
-    _classCallCheck(this, Client);
+var Color = function () {
+	function Color(codeColor, nameColor) {
+		_classCallCheck(this, Color);
 
-    this.name = name;
-    this.email = email;
-};
+		this.codeColor = codeColor;
+		this.nameColor = nameColor;
+	}
+
+	_createClass(Color, [{
+		key: 'getColor',
+		value: function getColor() {
+			return { codeColor: this.codeColor, nameColor: this.nameColor };
+		}
+	}]);
+
+	return Color;
+}();
+
+var red = new Color('red', 'RED');
+console.log(red.getColor());
+/*var bob = {
+	_name: 'Bob',
+	_friends: [ 'Guilherme', 'Camila', 'Murilo'],
+	printFriends: function printFriends(){
+		var self = this;
+		this._friends.forEach(function(f){
+			return console.log(self._name + " knows " + f);
+		});
+	}
+}
+
+bob.printFriends();
+
+var color = () => console.log('red');
+color();
+
+var bob = {
+	_name: 'Bob',
+	_friends: [ 'Guilherme', 'Camila', 'Murilo'],
+	printFriends: function printFriends(){
+		this._friends.forEach(f => 
+			console.log(this._name + " knows " + f));
+	}
+}
+
+bob.printFriends();*/
+"use strict";
--- course/src/app.js
+++ course/src/app.js
@@ -1,6 +1,13 @@
-class Client {
-    constructor (name, email) {
-        this.name = name;
-        this.email = email;
-    }
+class Color {
+	constructor(codeColor, nameColor){
+		this.codeColor = codeColor;
+		this.nameColor = nameColor;
+	};
+
+	getColor(){
+		return { codeColor: this.codeColor, nameColor: this.nameColor };
+	};
 }
+
+let red = new Color('red', 'RED');
+console.log(red.getColor());
