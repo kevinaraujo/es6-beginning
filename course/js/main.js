bc45ea282012c9e6081aa82209a38325cf925b99
Kevin Araújo <kevin.araujo@pravaler.com.br>
2019-09-09 10:55:46 -0300

    using string template

--- course/js/main.js
+++ course/js/main.js
@@ -1,29 +1,10 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var name = 'Guilherme';
+var age = 19;
+var text = '\n\tname: ' + name + ' \n\tage: ' + age + '  \n';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Color = function () {
-	function Color(codeColor, nameColor) {
-		_classCallCheck(this, Color);
-
-		this.codeColor = codeColor;
-		this.nameColor = nameColor;
-	}
-
-	_createClass(Color, [{
-		key: 'getColor',
-		value: function getColor() {
-			return { codeColor: this.codeColor, nameColor: this.nameColor };
-		}
-	}]);
-
-	return Color;
-}();
-
-var red = new Color('red', 'RED');
-console.log(red.getColor());
+console.log(text);
 /*var bob = {
 	_name: 'Bob',
 	_friends: [ 'Guilherme', 'Camila', 'Murilo'],
@@ -50,4 +31,18 @@ var bob = {
 }
 
 bob.printFriends();*/
+"use strict";
+/*class Color {
+	constructor(codeColor, nameColor){
+		this.codeColor = codeColor;
+		this.nameColor = nameColor;
+	};
+
+	getColor(){
+		return { codeColor: this.codeColor, nameColor: this.nameColor };
+	};
+}
+
+let red = new Color('red', 'RED');
+console.log(red.getColor());*/
 "use strict";
---
+++ course/src/app2.js
@@ -0,0 +1,13 @@
+/*class Color {
+	constructor(codeColor, nameColor){
+		this.codeColor = codeColor;
+		this.nameColor = nameColor;
+	};
+
+	getColor(){
+		return { codeColor: this.codeColor, nameColor: this.nameColor };
+	};
+}
+
+let red = new Color('red', 'RED');
+console.log(red.getColor());*/
